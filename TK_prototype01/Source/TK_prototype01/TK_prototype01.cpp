1f5f8250b395756895ef990863ce4e358375a2a1
Voclost <zdourai@gmail.com>
2018-01-22 23:36:16 +0100

    TK_INSTALL 03 LOCAL

---
+++ TK_prototype01/Source/TK_prototype01/TK_prototype01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TK_prototype01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TK_prototype01, "TK_prototype01" );
---
+++ TK_prototype01/Source/TK_prototype01/TK_prototype01GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TK_prototype01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TK_PROTOTYPE01_API ATK_prototype01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
